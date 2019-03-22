b5d02be75bcefb902e42bf4570b211b4e5134e69
Katya Astakhova <KatyaAstakhova@coherentsolutions.com>
2019-03-22 10:42:56 +0300

    Task130

---
+++ Task130/Methods.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace Task130
+{
+    public class Methods
+    {
+        public IWebDriver NavigateTo(string url)
+        {
+            var chromeDriver = new ChromeDriver(@"D:\Automation");
+            chromeDriver.Navigate().GoToUrl(url);
+            return chromeDriver;
+        }
+
+        public void LoginUser(IWebDriver chromeDriver, string username, string password)
+        {
+            Actions builder = new Actions(chromeDriver);
+            chromeDriver.FindElement(By.LinkText("Войти")).Click();
+            chromeDriver.FindElement(By.Name("login")).SendKeys(username);
+            builder.SendKeys(Keys.Tab).Perform();
+            chromeDriver.FindElement(By.Name("password")).SendKeys(password);
+            chromeDriver.FindElement(By.ClassName("auth__enter")).Click();
+        }
+
+        public bool IsUserLogin(IWebDriver chromeDriver)
+        {
+            return chromeDriver.FindElement(By.ClassName("uname")).Displayed;
+        }
+
+        public void LogoutUser(IWebDriver chromeDriver, string username, string password)
+        {
+            LoginUser(chromeDriver,username,password);
+            chromeDriver.FindElement(By.ClassName("uname")).Click();
+            chromeDriver.FindElement(By.LinkText("Выйти")).Click();
+        }
+
+        public bool IsUserLogout(IWebDriver chromeDriver)
+        {
+            return chromeDriver.FindElement(By.LinkText("Войти")).Displayed;
+        }
+    }
+}
---
+++ Task130/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Task130
+{
+    [TestFixture]
+    public class Tests
+    {
+        public const string url = "https://www.tut.by/";
+        public const string username = "seleniumtests@tut.by";
+        public const string password = "123456789zxcvbn";
+
+        [Test]
+        public void Login()
+        {
+            Methods methods = new Methods();
+            var driver = methods.NavigateTo(url);
+            methods.LoginUser(driver, username, password);
+            Assert.IsTrue(methods.IsUserLogin(driver));
+        }
+
+        [Test]
+        public void Logout()
+        {
+            Methods methods = new Methods();
+            var driver = methods.NavigateTo(url);
+            methods.LogoutUser(driver, username, password);
+            Assert.IsTrue(methods.IsUserLogout(driver));
+        }
+    }
+}
